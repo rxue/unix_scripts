d188bf3015c5ce2915fac74a650e2598d88bc8ff
ruixueru <rui.xue@affecto.com>
2016-04-05 10:59:12 +0300

    wrapper modified tyyli

--- wrapper_inotifywait.sh
+++ wrapper_inotifywait.sh
@@ -3,98 +3,98 @@ print_usage() {
 	echo "Usage: ${BASH_SOURCE}	-f <path of the file to watch> -c <command or script with arguments to execute> [-l loop or not ] [-s the command is a script name ] [-t <seconds> sleep time in seconds after a successful execution ]"
 	exit 1
 }
-SEC=1
+sec=1
 while getopts ":f:c:el:ost:" opt; do
-	case $opt in
+	case ${opt} in
 		f)
-			FILE="$OPTARG"
+			file="${OPTARG}"
 			;;  
 		c)
-			SCRIPT_CMD="$OPTARG"
+			script_cmd="${OPTARG}"
 			;;
 		e)
-			LOOP=true
+			loop=true
 			;;
 		l)
-			LOG_FILE="$OPTARG"
+			log_file="${OPTARG}"
 			;;
 		o)
-			OVERWRITE=true
+			overwrite=true
 			;;
 		s)
-			SCRIPT=true
+			script=true
 			;;
 		t)
-			SEC=$OPTARG
+			sec=${OPTARG}
 			;;
 		\?)
-			echo "Invalid option: -$OPTARG" >&2
+			echo "Invalid option: -${OPTARG}" >&2
 			print_usage
 			;;
 		:)
-			echo "Option -$OPTARG requires an argument." >&2
+			echo "Option -${OPTARG} requires an argument." >&2
 			print_usage
 			;;
 		esac
 done
 
 validate() {
-	if [ -z "$FILE" ]; then
+	if [ -z "${file}" ]; then
 		echo "File to watch not given" >&2
 		print_usage
 	fi
-	DIR="$(dirname "$FILE")"
-	if [ ! -d "$DIR" ]; then
+	DIR="$(dirname "${file}")"
+	if [ ! -d "${DIR}" ]; then
 		echo "Directory of the given file path does not exist" >&2
 		print_usage
 	fi
-	if [ -z "$SCRIPT_CMD" ]; then
+	if [ -z "${script_cmd}" ]; then
 		echo "The command is not given" >&2
 		print_usage
 	fi
 }
-if [ "${OVERWRITE}" = true ]; then
-	> $LOG_FILE
+if [ "${overwrite}" = true ]; then
+	> ${log_file}
 fi
-# predetermined var for logger function: LOG_FILE file to log to
+# predetermined var for logger function: log_file file to log to
 # predetermined var for logger function: overwrite or append (default)
 #
 # $1 log level - INFO ERROR DEBUG
 # $2 log message
 # $3 shell line number
 logger() {
-	echo "$(date) $1 [${BASH_SOURCE}:$3] $2" >> $LOG_FILE
+	echo "$(date) $1 [${BASH_SOURCE}:$3] $2" >> $log_file
 }
 validate
-eval FILE="$FILE"
+eval file="$file"
 #NOTE! I am not sure if the two boolean operations connected by && is atomic or not, if not this is a bug
 while true; do
   # watch and wait
-	if [ ! -f $FILE ] && inotifywait --format '%e %f : %w' $DIR; then
+	if [ ! -f $file ] && inotifywait --format '%e %f : %w' $DIR; then
 		EXEC=true 
-	#elif [ -f $FILE ] && inotifywait -e modify,delete_self,move_self --format '%e %f : %w' $FILE; then
-	elif [ -f $FILE ] && inotifywait --format '%e %f : %w' $FILE; then
+	#elif [ -f $file ] && inotifywait -e modify,delete_self,move_self --format '%e %f : %w' $file; then
+	elif [ -f $file ] && inotifywait --format '%e %f : %w' $file; then
 		EXEC=true
 	fi
-	sleep ${SEC}
+	sleep ${sec}
 	# execute
-	if [ "$EXEC" = true -a -f $FILE ]; then 
-		if [ "$SCRIPT" = true ]; then			
-			/bin/bash ${SCRIPT_CMD}
+	if [ "$EXEC" = true -a -f $file ]; then 
+		if [ "$script" = true ]; then			
+			/bin/bash ${script_cmd}
 			EXIT_CODE=$?
 			LINE_NO=$(expr ${LINENO} - 2)
 		else
-			eval "${SCRIPT_CMD}"
+			eval "${script_cmd}"
 			EXIT_CODE=$?
 			LINE_NO=$(expr ${LINENO} - 2)
 		fi
 		if [ $EXIT_CODE -eq 0 ]; then
-			logger INFO "COMMAND or SCRIPT - ${SCRIPT_CMD} - execution succeeded :)" $LINE_NO
+			logger INFO "COMMAND or script - ${script_cmd} - execution succeeded :)" $LINE_NO
 		else
-			logger ERROR "COMMAND or SCRIPT - ${SCRIPT_CMD} - execution failed :<" $LINE_NO
+			logger ERROR "COMMAND or script - ${script_cmd} - execution failed :<" $LINE_NO
 		fi
 	fi
-	if [ "$LOOP" != true ]; then #This true is /bin/true
+	if [ "$loop" != true ]; then #This true is /bin/true
 		exit $EXIT_CODE
 	elif [ "$EXEC" = true ]; then
 		EXEC=false
