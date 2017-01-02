ef29e80e4423c1ec95d71dd02de6788e28181f79
Rui Xue <rxue@ruixue.mobi>
2017-01-02 22:41:25 +0200

    command funciton for installing Maria-DB added

--- configure_debian_functions.sh
+++ configure_debian_functions.sh
@@ -1,3 +1,4 @@
+#### Professional
 # Install and configure vim
 # Reference: http://vim.wikia.com/wiki/Indenting_source_code
 function install_vim {
@@ -51,6 +52,62 @@ function make_shortcut {
 /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/${custom_name}/ binding "${3}"  
 }
 
+# Add program to GNOME Main Menu on system level 
+# $1 - program command e.g. /usr/bin/eclipse
+# $2 - program icon absolute path e.g. /opt/eclipse/eclipse.xpm
+# $3 - program name
+function add_program_to_gnome_main_menu {
+  local _desktop_file_name="$(basename ${1})"
+  local  _program_name="${3}"
+  local _desktop_file_content=`cat <<EOF
+[Desktop Entry]
+Comment=
+Terminal=false
+Name=${_program_name}
+Exec=${1}
+Type=Application
+Icon=${2}
+EOF`
+  echo "${_desktop_file_content}" |tee /usr/share/applications/${_desktop_file_name}.desktop
+}
+# Install Eclipse Neon
+# This function is calling the function add_program_to_gnome_main_menu
+function install_eclipse_installer {
+  if [ -z "$1" ]; then
+    local _installer_http_addr="https://www.eclipse.org/downloads/download.php?file=/oomph/epp/neon/R2a/eclipse-inst-linux64.tar.gz"
+  fi
+  local _actual_http_addr=$(wget --server-response --spider "${_installer_http_addr}" 2>&1 |grep "^Location:" |awk '{print $2}')
+  wget "${_actual_http_addr}"
+  tar -xvzf $(basename ${_actual_http_addr})
+  rm $(basename ${_actual_http_addr})
+  eclipse-installer/eclipse-inst
+  # NB! When using the installer to install Eclipse IDE, turn off the "BUNDLE POOLS" on the upper righ corner of the installer GUI
+  # Refer to http://stackoverflow.com/questions/37864572/using-different-location-for-eclipses-p2-file
+  ln -fs /opt/eclipse/eclipse /usr/bin/eclipse
+  add_program_to_gnome_main_menu /opt/eclipse/eclipse /opt/eclipse/icon.xpm "Eclipse-Neon"
+}
+# Install Maria-DB
+# Refer to https://downloads.mariadb.org/mariadb/repositories/#mirror=netinch&distro=Debian&distro_release=jessie--jessie&version=10.1 
+function install_mariadb {
+  sudo apt-get install software-properties-common
+  sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+  sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirror.netinch.com/pub/mariadb/repo/10.1/debian jessie main'
+  sudo apt-get update
+  sudo apt-get install mariadb-server
+}
+
+function install_postfix {
+  debconf-set-selections <<< "postfix postfix/mailname string example.com"
+  debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+  apt-get install -y postfix
+  sed -i "s/inet_interfaces =.*$/inet_interfaces = loopbak-only/" /etc/postfix/main.cf 
+}
+
+function install_system_monitors {
+  apt-get install strace
+  apt-get install htop
+}
+#### Home
 # Install Chrome
 function install_chrome {
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -94,51 +151,4 @@ function install_skype {
   dpkg -i $(basename ${download_url})
   apt-get install -f
   dpkg -i $(basename ${download_url})
-}
-
-# Add program to GNOME Main Menu on system level 
-# $1 - program command e.g. /usr/bin/eclipse
-# $2 - program icon absolute path e.g. /opt/eclipse/eclipse.xpm
-# $3 - program name
-function add_program_to_gnome_main_menu {
-  local _desktop_file_name="$(basename ${1})"
-  local  _program_name="${3}"
-  local _desktop_file_content=`cat <<EOF
-[Desktop Entry]
-Comment=
-Terminal=false
-Name=${_program_name}
-Exec=${1}
-Type=Application
-Icon=${2}
-EOF`
-  echo "${_desktop_file_content}" |tee /usr/share/applications/${_desktop_file_name}.desktop
-}
-
-# Install Eclipse Neon
-# This function is calling the function add_program_to_gnome_main_menu
-function install_eclipse_installer {
-  if [ -z "$1" ]; then
-    local _installer_http_addr="https://www.eclipse.org/downloads/download.php?file=/oomph/epp/neon/R2a/eclipse-inst-linux64.tar.gz"
-  fi
-  local _actual_http_addr=$(wget --server-response --spider "${_installer_http_addr}" 2>&1 |grep "^Location:" |awk '{print $2}')
-  wget "${_actual_http_addr}"
-  tar -xvzf $(basename ${_actual_http_addr})
-  rm $(basename ${_actual_http_addr})
-  eclipse-installer/eclipse-inst
-  # NB! When using the installer to install Eclipse IDE, turn off the "BUNDLE POOLS" on the upper righ corner of the installer GUI
-  # Refer to http://stackoverflow.com/questions/37864572/using-different-location-for-eclipses-p2-file
-  ln -fs /opt/eclipse/eclipse /usr/bin/eclipse
-  add_program_to_gnome_main_menu /opt/eclipse/eclipse /opt/eclipse/icon.xpm "Eclipse-Neon"
-}
-
-function install_postfix {
-  debconf-set-selections <<< "postfix postfix/mailname string example.com"
-  debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-  apt-get install -y postfix
-  sed -i "s/inet_interfaces =.*$/inet_interfaces = loopbak-only/" /etc/postfix/main.cf 
-}
-function install_system_monitors {
-  apt-get install strace
-  apt-get install htop
-}
+} 
