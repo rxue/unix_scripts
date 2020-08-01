9cef07607515c61b74b1e892d1d1a39da65fff70
dingding <ruixue.fi@gmail.com>
2020-08-01 13:46:58 +0300

    devops functions

--- devops_funcs.sh
+++ devops_funcs.sh
@@ -1,3 +1,7 @@
+# @param 1 - authorizatio data, currently basic loginemail:password
+# @param 2 - project key in Bitbucket
+# @param 3 - workspace, i.e. user name
+# @param 4 - repository name
 # reference: https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Bworkspace%7D/%7Brepo_slug%7D#post
 function create_repo_in_bitbucket {
   _auth_data=${1}
@@ -7,7 +11,7 @@ function create_repo_in_bitbucket {
   curl --user ${_auth_data} -X POST -H "Content-Type: application/json" -d '{
     "scm": "git",
     "project": {
-        "key": "${_project_key}"
+        "key": "'${_project_key}'"
     }
-  }' https:/k/api.bitbucket.org/2.0/repositories/${_workspace}/${_repo}
+  }' https://api.bitbucket.org/2.0/repositories/${_workspace}/${_repo}
 }
