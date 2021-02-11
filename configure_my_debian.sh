ffe9c05953cbcb08cf892f7500446f7b115b368e
Rui Xue <ruixue.fi@gmail.com>
2021-02-12 01:48:43 +0200

    Python script for make_shortcut added and can work, but currently not idempotent yet

--- configure_my_debian.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-# Resolving the bug - "Media change: please insert the disc labeled" - when using apt to install software
-sudo sed -i '/cdrom/d' /etc/apt/sources.list
-add_program_to_gnome_main_menu "/usr/bin/eclipse" "/opt/eclipse/java-neon/eclipse/icon.xpm" "Eclipse Neon"   
-user_1000=`getent passwd 1000 |cut -d':' -f1`
-#runuser -l ${user_1000} -c 'wget http://ftp.fau.de/eclipse/che/eclipse-che-4.0.1.zip'
-#gpasswd -a ${user_1000} docker
--- debian_configuration/make_shortcut.py
+++ debian_configuration/make_shortcut.py
@@ -3,19 +3,35 @@ import sys
 from gi.repository import Gio
 from gi.repository import GLib
 #from ast import literal_eval
-def _get_custom_path(name:str):
-  custom_name = name.replace(' ', '')
-  return "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/" + custom_name
+media_keys_schema = 'org.gnome.settings-daemon.plugins.media-keys' 
+custom_keybinding_schema = media_keys_schema + '.custom-keybinding'
+def _get_new_custom_keybinding_path(name:str):
+  custom_name = name.replace(' ', '').lower()
+  return "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/" + custom_name + '/'
 
-def make_shortcut(name:str):
-  mediaKeys = Gio.Settings.new('org.gnome.settings-daemon.plugins.media-keys')
+def _add_new_custom_keybinding_path(new_custom_keybinding_path:str):
+  global media_keys_schema
+  media_keys_settings = Gio.Settings.new(media_keys_schema)
   keyCustomKeyBindings = 'custom-keybindings'
-  customKeyBindings = mediaKeys.get_value(keyCustomKeyBindings)
+  customKeyBindings = media_keys_settings.get_value(keyCustomKeyBindings)
   paths = customKeyBindings.get_strv()
-  newCustomSettingPath = _get_custom_path(name)
-  paths.append(newCustomSettingPath)
-  print("DEBUG:",paths)
-  mediaKeys.set_value(keyCustomKeyBindings, GLib.Variant.new_strv(paths))
+  paths.append(new_custom_keybinding_path)
+  media_keys_settings.set_value(keyCustomKeyBindings, GLib.Variant.new_strv(paths))
+  
+def make_shortcut(name:str,command:str,shortcut:str):
+  global media_keys_schema 
+  new_custom_keybinding_path = _get_new_custom_keybinding_path(name)
+  _add_new_custom_keybinding_path(new_custom_keybinding_path)
+  global custom_keybinding_schema
+  custom_keybinding_settings = Gio.Settings.new_with_path(custom_keybinding_schema, new_custom_keybinding_path)
+  custom_keybinding_settings.set_string('name', name)
+  custom_keybinding_settings.set_string('command', command)
+  custom_keybinding_settings.set_string('binding', shortcut)
+  custom_keybinding_settings.apply()
+  Gio.Settings.sync()
+  
 
 new_operation_name = sys.argv[1]
-make_shortcut(new_operation_name)
+command = sys.argv[2]
+shortcut = sys.argv[3]
+make_shortcut(new_operation_name, command, shortcut)
