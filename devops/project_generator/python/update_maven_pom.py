645d0a5972d56dae25c9e6eb66151bfa3592342d
Rui Xue <ruixue.fi@gmail.com>
2021-02-28 23:32:40 +0200

    Python script

--- devops/project_generator/python/update_maven_pom.py
+++ devops/project_generator/python/update_maven_pom.py
@@ -2,17 +2,17 @@ import xml.etree.ElementTree as ET
 import re
 import sys
 namespace_key = 'n'
-def _get_namespace(tree):
+def _get_namespace(tree:ET)->str:
   root_tag = tree.findall(".")[0].tag
   return re.search("(?<={).*(?=})", root_tag).group(0)
 
-def _update_java_version(tree, namespaces):
+def _update_java_version(tree:ET, namespaces:dict):
   global namespace_key
   java_version = '1.8'
   tree.findall('./'+namespace_key+':properties/'+namespace_key+':maven.compiler.source', namespaces)[0].text = java_version
   tree.findall('./'+namespace_key+':properties/'+namespace_key+':maven.compiler.target', namespaces)[0].text = java_version
 
-def _remove_junit_dependency(tree, namespaces): 
+def _remove_junit_dependency(tree:ET, namespaces:dict): 
   global namespace_key
   xpath_dependencies = './'+namespace_key+':dependencies'
   dependencies = tree.findall(xpath_dependencies, namespaces)[0]
