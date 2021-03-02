ed2f1d2b2a133317ec7b731708dd88fb7c499923
Rui Xue <ruixue.fi@gmail.com>
2021-03-02 23:26:43 +0200

    add dependency element

--- devops/project_generator/python/update_maven_pom.py
+++ devops/project_generator/python/update_maven_pom.py
@@ -17,17 +17,35 @@ def update_maven_pom(pom_xml_path:str):
     java_version = '1.8'
     tree.findall('./'+namespace_key+':properties/'+namespace_key+':maven.compiler.source', namespaces)[0].text = java_version
     tree.findall('./'+namespace_key+':properties/'+namespace_key+':maven.compiler.target', namespaces)[0].text = java_version
+  
+  xpath_dependencies = './'+namespace_key+':dependencies'
+  dependencies = tree.findall(xpath_dependencies, namespaces)[0]
 
   def _remove_junit_dependency(): 
-    xpath_dependencies = './'+namespace_key+':dependencies'
-    dependencies = tree.findall(xpath_dependencies, namespaces)[0]
     junit_dependency = tree.findall(xpath_dependencies+'/'+namespace_key+':dependency', namespaces)[0]
     dependencies.remove(junit_dependency)
 
   _update_java_version()
   _remove_junit_dependency()
+  indent = '  '
+  dependency_element = ET.Element('dependency')
+  dependency_element.text = '\n' + indent*3
+  dependency_element.tail = '\n' + indent
+  groupid_element = ET.Element('groupId')
+  groupid_element.text = 'org.junit.jupiter'
+  groupid_element.tail = '\n' + indent*3
+  dependency_element.append(groupid_element)
+  artifactid_element = ET.Element('artifactId')
+  artifactid_element.text = 'org.junit.jupiter-api'
+  artifactid_element.tail = '\n' + indent*3
+  dependency_element.append(artifactid_element)
+  version_element = ET.Element('version')
+  version_element.text = '1.1'
+  version_element.tail = '\n'
+  dependency_element.append(version_element)
+  dependencies.append(dependency_element)
   ET.register_namespace('',namespace)
-  tree.write(pom_xml_path) 
+  tree.write(pom_xml_path, xml_declaration=True) 
 
 if __name__ == "__main__":
   update_maven_pom(sys.argv[1])
