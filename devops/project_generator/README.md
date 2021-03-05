e6bc31628f2d4824fbdea90123c9ff3ad2b0bf4f
Rui Xue <ruixue.fi@gmail.com>
2021-03-06 00:37:18 +0200

    activate.sh for project_generator can work already

---
+++ devops/project_generator/README.md
@@ -0,0 +1,11 @@
+# What
+This simple script can:
+
+* `generate_maven`: generate `maven` an empty project with the state of art dependencies, e.g. junit 5 etc.
+
+# Pre-requisite:
+1. `cd` to whereever the project wanted to be generated
+2. `source` this `activate.sh` in that directory
+
+# How to run `generate_maven` 
+Example: `generate_maven rx.practice test-project`, where `rx.practice` is the group ID, `test-project` is the name of the new maven project 
--- devops/project_generator/activate.sh
+++ devops/project_generator/activate.sh
@@ -6,4 +6,8 @@ function generate_maven {
   name=$2
   mvn archetype:generate -DgroupId=${group_id} -DartifactId=${name} -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
   python3 ${SCRIPT_DIR}/python/generate_maven_pom.py ${name}
+  # remove automatically generated java class files
+  package_dir=$(python3 -c "print('${group_id}'.replace('.','/'))")
+  rm ${name}/src/main/java/${package_dir}/*.java
+  rm ${name}/src/test/java/${package_dir}/*.java
 }
