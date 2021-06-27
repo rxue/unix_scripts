671562a21e50a44bf56052380c0d585c60aacd59
Rui Xue <ruixue.fi@gmail.com>
2021-06-28 00:35:57 +0300

    Decided to use PyPDF4 lieu of PdfPlumber

--- python/venv/pdftool/SearchFromPdf.py
+++ python/venv/pdftool/SearchFromPdf.py
@@ -1,16 +1,29 @@
 import sys
-import pdfplumber
-def searchFromFile(path:str,keyword:str) -> list:
-    with pdfplumber.open(path) as pdf:
-        result = []
-        for page in pdf.pages:
-            pageText = page.extract_text()
-            if pageText != None and keyword in pageText:
-                result.append(page)
-        return result
+from typing import List
+import PyPDF4 as pypdf
+from PyPDF4.pdf import PageObject
+
+def searchFromFile(path:str,keyword:str) -> List[str]:
+    pdf = pypdf.PdfFileReader(open(path, "rb"))
+    if pdf.isEncrypted:
+        pdf.decrypt('')
+    numberOfPages = pdf.getNumPages()
+    result = [str]
+    for pageNumber in range(0,numberOfPages):
+        page = pdf.getPage(pageNumber)
+        text = page.extractText()
+        if keyword in text:
+            result.append(text)
+    return result
+    #try:
+    #    page_label_type = pdf.trailer["/Root"]["/PageLabels"]["/Nums"]
+    #    print(page_label_type.getObject())
+    #    print(page_label_type[3].getObject())
+    #except:
+    #    print("No /PageLabel object")
 
 if __name__ == '__main__':
     resultList = searchFromFile(sys.argv[1], sys.argv[2])
-    #resultList = searchFromFile("/home/rxue/Documents/Books/Programming/refactor/WorkingEffectivelyWithLegacyCode.pdf", "test")
     for page in resultList:
-        print("page: ",page.page_number)
+        print("page content:",page)
+        print("\n\n\n")     
