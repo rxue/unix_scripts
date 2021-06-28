9d371892b05c720579a1d6b59dc6f82188e9fa84
Rui Xue <ruixue.fi@gmail.com>
2021-06-29 00:17:15 +0300

    search from pdf function not ready yet,   adding function to get the logical page number string

--- python/venv/pdftool/SearchFromPdf.py
+++ python/venv/pdftool/SearchFromPdf.py
@@ -1,29 +1,50 @@
 import sys
 from typing import List
 import PyPDF4 as pypdf
-from PyPDF4.pdf import PageObject
+from PyPDF4.generic import NumberObject
+from PyPDF4.pdf import PageObject, PdfFileReader
 
-def searchFromFile(path:str,keyword:str) -> List[str]:
+def _getLogicalPageNumber(pageLabelNums:list,pageIndex:int):
+    leftBound = -1
+    rightBound = -1
+    bothBoundsFound = False
+    dicionary = None
+    for pageLabelNum in pageLabelNums:
+        if type(pageLabelNum) is NumberObject:
+            if pageLabelNum <= pageIndex:
+                leftBound = pageLabelNum
+                if pageLabelNum == pageIndex:
+                    rightBound = pageLabelNum
+                    bothBoundsFound = True
+            else:
+                rightBound = pageLabelNum
+                break
+        else:
+            dictionary = pageLabelNum.getObject()
+            if bothBoundsFound:
+                break
+    key = dictionary.keys()
+    if '/P' in key:
+                print(dictionary['/P'])
+        
+
+def searchFromFile(path:str,keyword:str) -> List[PageObject]:
     pdf = pypdf.PdfFileReader(open(path, "rb"))
     if pdf.isEncrypted:
         pdf.decrypt('')
     numberOfPages = pdf.getNumPages()
-    result = [str]
-    for pageNumber in range(0,numberOfPages):
-        page = pdf.getPage(pageNumber)
+    pageLabelNums = pdf.trailer["/Root"]["/PageLabels"]["/Nums"]
+    print(pageLabelNums)
+    result = []
+    for pageIndex in range(0,numberOfPages):
+        page = pdf.getPage(pageIndex)
         text = page.extractText()
         if keyword in text:
-            result.append(text)
+            result.append(page)
     return result
-    #try:
-    #    page_label_type = pdf.trailer["/Root"]["/PageLabels"]["/Nums"]
-    #    print(page_label_type.getObject())
-    #    print(page_label_type[3].getObject())
-    #except:
-    #    print("No /PageLabel object")
-
+    
 if __name__ == '__main__':
     resultList = searchFromFile(sys.argv[1], sys.argv[2])
-    for page in resultList:
-        print("page content:",page)
-        print("\n\n\n")     
+    #for page in resultList:
+    #    print("page content:",page.extractText())
+    #    print("\n\n\n")     
