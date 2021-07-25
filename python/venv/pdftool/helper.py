a3ae8295e572ee5919dec12fd73c9ed7a0cdd3b2
rui <ruixue.fi@gmail.com>
2021-07-25 23:05:49 +0300

    Issue: #6  implemented intToRoman with TDD

--- python/venv/pdftool/helper.py
+++ python/venv/pdftool/helper.py
@@ -1,5 +1,19 @@
 """
-int allowed - [1,1999]
+int allowed - [1,3999]
+reference: https://www.youtube.com/watch?v=yzB4M-UXqgI
 """
 def intToRoman(integer:int) -> str:
-    return None
+    map = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC',50:'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
+    if not 1 <= integer <= 3999:
+        raise ValueError('Given integer has to be in range [1,3999]')
+    result = None
+    remainder = integer
+    for key in map:
+        singleResult = remainder // key
+        if result is None:
+            if singleResult > 0:
+                result = map[key] * singleResult
+        elif singleResult > 0:
+            result = result + map[key] * singleResult  
+        remainder %= key
+    return result
--- python/venv/pdftool/tests/HelperTest.py
+++ python/venv/pdftool/tests/HelperTest.py
@@ -8,9 +8,17 @@ import unittest
 
 class TestHelper(unittest.TestCase):
     def test_intToRoman(self):
+        with self.assertRaises(ValueError):
+            intToRoman(0)
         self.assertEqual(intToRoman(1), 'I')
+        self.assertEqual(intToRoman(2), 'II')
         self.assertEqual(intToRoman(4), 'IV')
+        self.assertEqual(intToRoman(5), 'V')
+        self.assertEqual(intToRoman(6), 'VI')
+        self.assertEqual(intToRoman(7), 'VII')
         self.assertEqual(intToRoman(2944), 'MMCMXLIV')
+        with self.assertRaises(ValueError):
+            intToRoman(4000)
 
 if __name__ == '__main__':
     unittest.main()
